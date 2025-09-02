4c14cdb0b8603ab3aae71b3091fda7e5994b107f
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2025-09-02 11:15:15 -0700

    gsap animations

---
+++ script.js
@@ -0,0 +1,31 @@
+gsap.to("#box1", {
+    x: 1200,
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    backgroundColor: "blue",
+    borderRadius: "50%",
+    scale: "0.5",
+    repeat: Infinity 
+})
+
+// gsap.from("#box2", {
+//     x:500,
+//     y:500,
+//     duration:2,
+//     delay:1
+// })
+
+// gsap.from("h1", {
+//     // color: "red",
+//     opacity: 0,
+//     y:30,
+//     duration: 1,
+//     delay: 1,
+//     stagger: 0.3
+//     // stagger: -1 // ulta chalega
+// })
+
+// Animation --> it is all about intial to final 
+// to --> final pr changes aajayengi
+// from --> initial pr changes aajayengi
